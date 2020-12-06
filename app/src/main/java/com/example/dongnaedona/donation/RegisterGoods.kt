1a44a43c13ad34bb427829ccddfad4651643b7aa
milkcoke <mbh023@konkuk.ac.kr>
2020-12-06 18:23:16 +0900

    ADD register goods activity (not yet column) + photo editing

---
+++ app/src/main/java/com/example/dongnaedona/donation/RegisterGoods.kt
@@ -0,0 +1,12 @@
+package com.example.dongnaedona.donation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dongnaedona.R
+
+class RegisterGoods : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_goods)
+    }
+}
